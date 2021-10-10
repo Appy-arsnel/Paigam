39704acb46d68d53961bf50d1dbc6df78a8efbfe
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-10-10 12:45:04 +0530

    Initial commit
    screen 3 ui/ux almost done

--- app/src/main/java/com/example/paigam/onboarding_page1.java
+++ app/src/main/java/com/example/paigam/onboarding_page1.java
@@ -5,17 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.paigam.onboarding.onboarding_page2;
 
 public class onboarding_page1 extends AppCompatActivity {
-     ImageView loginbutton;
+     Button loginbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_page1);
-        loginbutton=findViewById(R.id.loginpag1);
+        loginbutton=findViewById(R.id.login2button);
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
