9d10765a42b1ff5ce3bb1578d8ff94cd43cfb4e1
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-10-22 02:28:19 +0530

    Made few changes in ui

--- app/src/main/java/com/example/paigam/onboarding/onboarding_page3.java
+++ app/src/main/java/com/example/paigam/onboarding/onboarding_page3.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.paigam.R;
+
 public class onboarding_page3 extends AppCompatActivity {
 
     @Override
